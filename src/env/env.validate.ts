c2812b8e21678d0ec906ec22d571491ba2f069f9
MauroRaya <maurorayabr@gmail.com>
2026-01-27 00:35:39 -0300

    feat: create env validate function for config service

---
+++ src/env/env.validate.ts
@@ -0,0 +1,44 @@
+import { plainToInstance } from "class-transformer";
+import {
+  IsNotEmpty,
+  IsPort,
+  validateSync
+} from "class-validator";
+
+class EnvironmentVariables {
+  @IsNotEmpty()
+  POSTGRES_HOST: string;
+
+  @IsPort()
+  POSTGRES_PORT: number;
+
+  @IsNotEmpty()
+  POSTGRES_USER: string;
+
+  @IsNotEmpty()
+  POSTGRES_PASSWORD: string;
+
+  @IsNotEmpty()
+  POSTGRES_DB: string;
+
+  @IsPort()
+  PORT: number;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(
+    EnvironmentVariables, 
+    config,
+  );
+
+  const errors = validateSync(
+    validatedConfig, 
+    { skipMissingProperties: false }
+  );
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+
+  return validatedConfig;
+}
