8b3ce63dde96d6bb2f2bdaa9489115e235365763
MauroRaya <maurorayabr@gmail.com>
2026-01-27 00:43:05 -0300

    feat: add auth module

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+import { ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET')
+      })
+    })
+  ],
+  controllers: [AuthController],
+  providers: [AuthService]
+})
+export class AuthModule {}
