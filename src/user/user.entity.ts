265224b7a2e26a3e32f1b070efbd82eda5bd603d
MauroRaya <maurorayabr@gmail.com>
2026-01-27 00:38:02 -0300

    feat: add user entity

---
+++ src/user/user.entity.ts
@@ -0,0 +1,20 @@
+import { 
+  Column, 
+  Entity, 
+  PrimaryGeneratedColumn 
+} from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  salt: string;
+
+  @Column()
+  hash: string;
+}
