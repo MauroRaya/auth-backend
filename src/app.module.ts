f2e295c2424493d548613d48a819bbc47c036662
MauroRaya <maurorayabr@gmail.com>
2026-01-27 00:38:49 -0300

    feat: add typeorm module for root and inject config service for env resolution

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,29 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { validate } from './env/env.validate';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       validate
+    }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (ConfigService: ConfigService) => ({
+        type: 'postgres',
+        host: ConfigService.get<string>('POSTGRES_HOST'),
+        port: ConfigService.get<number>('POSTGRES_PORT'),
+        username: ConfigService.get<string>('POSTGRES_USER'),
+        password: ConfigService.get<string>('POSTGRES_PASSWORD'),
+        database: ConfigService.get<string>('POSTGRES_DB'),
+        entities: [User],
+        synchronize: false,
+      })
     }),
   ],
   controllers: [AppController],
