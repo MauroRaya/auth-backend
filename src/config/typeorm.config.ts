3eb0d84a54b52d32e8f1cdf4f5d74333c7ecb447
MauroRaya <maurorayabr@gmail.com>
2026-01-30 14:51:17 -0300

    refactor: change configService for envService in typeorm.config.ts

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,17 +1,20 @@
 import { DataSource } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
+import { EnvironmentVariables } from 'src/env/env.validate';
+import { EnvService } from 'src/env/env.service';
 import { config } from 'dotenv';
 config();
 
-const configService = new ConfigService();
+const configService = new ConfigService<EnvironmentVariables, true>();
+const envService = new EnvService(configService);
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: configService.get<string>('POSTGRES_HOST'),
-  port: configService.get<number>('POSTGRES_PORT'),
-  username: configService.get<string>('POSTGRES_USER'),
-  password: configService.get<string>('POSTGRES_PASSWORD'),
-  database: configService.get<string>('POSTGRES_DB'),
+  host: envService.get<string>('POSTGRES_HOST'),
+  port: envService.get<number>('POSTGRES_PORT'),
+  username: envService.get<string>('POSTGRES_USER'),
+  password: envService.get<string>('POSTGRES_PASSWORD'),
+  database: envService.get<string>('POSTGRES_DB'),
   synchronize: false,
   entities: ['**/*.entity.ts'],
   migrations: ['src/database/migrations/*-migration.ts'],
