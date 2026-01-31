5dc6d54447b8146b31e0520d8fe0ee419b9a0e8f
MauroRaya <maurorayabr@gmail.com>
2026-01-31 19:21:47 -0300

    feat: setup swagger for app in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { EnvService } from './env/env.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('API de autenticação')
+    .setDescription('Documentação da API de autenticação')
+    .setVersion('1.0.0')
+    .addBearerAuth()
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, documentFactory);
 
   const envService = app.get(EnvService);
   const port = envService.get<number>('PORT');
