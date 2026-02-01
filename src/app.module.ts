f875aedb0d565cac0aa38be9c48b4305b5a203fc
MauroRaya <maurorayabr@gmail.com>
2026-01-31 23:17:17 -0300

    feat: add health module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,7 @@ import { validate } from './env/env.validate';
 import { EnvModule } from './env/env.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './config/typeorm.config';
+import { HealthModule } from './health/health.module';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
@@ -16,6 +17,7 @@ import { AuthModule } from './auth/auth.module';
     TypeOrmModule.forRootAsync({
       useFactory: async () => AppDataSource.options,
     }),
+    HealthModule,
     AuthModule,
   ],
 })
