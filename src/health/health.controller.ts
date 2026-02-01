bf597a9f637fc5d639ed34d30de4664f4fbca607
MauroRaya <maurorayabr@gmail.com>
2026-01-31 23:27:16 -0300

    fix: fix check heap key

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -13,7 +13,7 @@ export class HealthController {
   @HealthCheck()
   async check() {
     return await this.health.check([
-      () => this.memory.checkHeap('memory heap', 150 * 1024 * 1024),
+      () => this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
       () => this.db.pingCheck('database'),
     ]);
   }
