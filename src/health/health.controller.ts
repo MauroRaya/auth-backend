1639dc5857b16e771a2b05d069987ae9dd18a502
MauroRaya <maurorayabr@gmail.com>
2026-01-31 23:43:25 -0300

    lint: routine lint in health controller

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,5 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
-import { DiskHealthIndicator, HealthCheck, HealthCheckService, MemoryHealthIndicator, TypeOrmHealthIndicator } from '@nestjs/terminus';
+import {
+  DiskHealthIndicator,
+  HealthCheck,
+  HealthCheckService,
+  MemoryHealthIndicator,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
@@ -14,7 +20,11 @@ export class HealthController {
   @HealthCheck()
   async check() {
     return await this.health.check([
-      async () => await this.disk.checkStorage('disk', { path: '/', thresholdPercent: 0.8 }),
+      async () =>
+        await this.disk.checkStorage('disk', {
+          path: '/',
+          thresholdPercent: 0.8,
+        }),
       async () => await this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
       async () => await this.memory.checkRSS('memory_rss', 150 * 1024 * 1024),
       async () => await this.db.pingCheck('database'),
