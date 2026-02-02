74011b58b7ac61dcccb92365d07b3bf089f64c46
MauroRaya <maurorayabr@gmail.com>
2026-02-01 23:51:46 -0300

    fix: decouple EnvironmentVariables from env.validate.ts file

--- src/env/env.validate.ts
+++ src/env/env.validate.ts
@@ -1,28 +1,6 @@
-import { IsNotEmpty, IsPort, validateSync } from 'class-validator';
 import { plainToInstance } from 'class-transformer';
-
-export class EnvironmentVariables {
-  @IsNotEmpty()
-  JWT_SECRET: string;
-
-  @IsNotEmpty()
-  POSTGRES_HOST: string;
-
-  @IsPort()
-  POSTGRES_PORT: number;
-
-  @IsNotEmpty()
-  POSTGRES_USER: string;
-
-  @IsNotEmpty()
-  POSTGRES_PASSWORD: string;
-
-  @IsNotEmpty()
-  POSTGRES_DB: string;
-
-  @IsPort()
-  PORT: number;
-}
+import { EnvironmentVariables } from './env';
+import { validateSync } from 'class-validator';
 
 export function validate(config: Record<string, unknown>) {
   const validatedConfig = plainToInstance(EnvironmentVariables, config);
