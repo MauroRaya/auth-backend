ef0d77ad8a75b5766c2af8476b7f626782848df5
MauroRaya <maurorayabr@gmail.com>
2026-02-02 22:22:56 -0300

    refactor(health): remove logic from controller, inject service instead

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -1,33 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
-import {
-  DiskHealthIndicator,
-  HealthCheck,
-  HealthCheckService,
-  MemoryHealthIndicator,
-  TypeOrmHealthIndicator,
-} from '@nestjs/terminus';
+import { HealthService } from './health.service';
+import { HealthCheck, HealthCheckResult } from '@nestjs/terminus';
 
 @Controller('health')
 export class HealthController {
-  constructor(
-    private readonly health: HealthCheckService,
-    private readonly disk: DiskHealthIndicator,
-    private readonly memory: MemoryHealthIndicator,
-    private readonly db: TypeOrmHealthIndicator,
-  ) {}
+  constructor(private readonly healthService: HealthService) {}
 
   @Get()
   @HealthCheck()
-  async check() {
-    return await this.health.check([
-      async () =>
-        await this.disk.checkStorage('disk', {
-          path: '/',
-          thresholdPercent: 0.8,
-        }),
-      async () => await this.memory.checkHeap('memory_heap', 150 * 1024 * 1024),
-      async () => await this.memory.checkRSS('memory_rss', 150 * 1024 * 1024),
-      async () => await this.db.pingCheck('database'),
-    ]);
+  async check(): Promise<HealthCheckResult> {
+    return await this.healthService.check();
   }
 }
