723a451ddc7620ba741ebb00be81ec840124783a
MauroRaya <maurorayabr@gmail.com>
2026-02-06 17:42:23 -0300

    feat: add sign in tests in auth service spec

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -3,7 +3,8 @@ import { AuthService } from './auth.service';
 import { JwtService } from '@nestjs/jwt';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { User } from 'src/user/user.entity';
-import { ConflictException } from '@nestjs/common';
+import { ConflictException, UnauthorizedException } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
 
 describe('AuthService', () => {
   let authService: AuthService;
@@ -62,5 +63,52 @@ describe('AuthService', () => {
     void expect(authService.signUp(email, password)).rejects.toThrow(
       ConflictException,
     );
+  });
+
+  it('sign in with non existing account expect exception', () => {
+    const email = 'johndoe@email.com';
+    const password = 'johndoe123';
+
+    usersRepositoryMock.findOneBy.mockResolvedValue(null);
+
+    void expect(authService.signIn(email, password)).rejects.toThrow(
+      UnauthorizedException,
+    );
+  });
+
+  it('sign in with non matching password expect exception', async () => {
+    const email = 'johndoe@email.com';
+    const password = 'johndoe123';
+
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(password, salt);
+
+    usersRepositoryMock.findOneBy.mockResolvedValue({
+      id: 1,
+      email,
+      salt,
+      hash,
+    });
+
+    const isPasswordValid = await bcrypt.compare(`${password}!`, hash);
+    expect(isPasswordValid).toBeFalsy();
+  });
+
+  it('sign in with matching password expect truthy', async () => {
+    const email = 'johndoe@email.com';
+    const password = 'johndoe123';
+
+    const salt = await bcrypt.genSalt();
+    const hash = await bcrypt.hash(password, salt);
+
+    usersRepositoryMock.findOneBy.mockResolvedValue({
+      id: 1,
+      email,
+      salt,
+      hash,
+    });
+
+    const isPasswordValid = await bcrypt.compare(password, hash);
+    expect(isPasswordValid).toBeTruthy();
   });
 });
