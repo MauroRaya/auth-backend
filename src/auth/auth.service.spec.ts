30c83afbc2db3f5f5f2ff15f381e8eddd1ee3f93
MauroRaya <maurorayabr@gmail.com>
2026-02-06 09:31:05 -0300

    feat: add sign in tests for auth service

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,21 +1,66 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { JwtService } from '@nestjs/jwt';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from 'src/user/user.entity';
+import { ConflictException } from '@nestjs/common';
 
 describe('AuthService', () => {
-  let service: AuthService;
+  let authService: AuthService;
+
+  const usersRepositoryMock = {
+    findOneBy: jest.fn(),
+    save: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [{
-        provide: AuthService,
-        useValue: {}
-      }],
+      providers: [
+        AuthService,
+        JwtService,
+        {
+          provide: getRepositoryToken(User),
+          useValue: usersRepositoryMock,
+        },
+      ],
     }).compile();
 
-    service = module.get<AuthService>(AuthService);
+    authService = module.get<AuthService>(AuthService);
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(authService).toBeDefined();
+  });
+
+  it('sign up with available email expect to be defined', async () => {
+    const email = 'johndoe@email.com';
+    const password = 'johndoe123';
+
+    usersRepositoryMock.findOneBy.mockResolvedValue(null);
+    usersRepositoryMock.save.mockResolvedValue({
+      id: 1,
+      email,
+      salt: 'salt',
+      hash: 'hash',
+    });
+
+    const user = await authService.signUp(email, password);
+    expect(user).toBeDefined();
+  });
+
+  it('sign up with unavailable email expect exception', () => {
+    const email = 'johndoe@email.com';
+    const password = 'johndoe123';
+
+    usersRepositoryMock.findOneBy.mockResolvedValue({
+      id: 1,
+      email,
+      salt: 'salt',
+      hash: 'hash',
+    });
+
+    void expect(authService.signUp(email, password)).rejects.toThrow(
+      ConflictException,
+    );
   });
 });
