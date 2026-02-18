7af22797c932da754693c99e8a12c0680420a29b
MauroRaya <maurorayabr@gmail.com>
2026-02-18 15:39:23 -0300

    feat(user): import bull module and user module in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { EnvModule } from './env/env.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './config/typeorm.config';
 import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { BullModule } from '@nestjs/bullmq';
+import { EnvService } from './env/env.service';
 
 @Module({
   imports: [
@@ -19,6 +22,18 @@ import { AuthModule } from './auth/auth.module';
       useFactory: () => AppDataSource.options,
     }),
     AuthModule,
+    BullModule.forRootAsync({
+      useFactory: (envService: EnvService) => {
+        return {
+          connection: {
+            host: envService.get<string>('REDIS_HOST'),
+            port: envService.get<number>('REDIS_PORT'),
+          },
+        };
+      },
+      inject: [EnvService],
+    }),
+    UserModule,
   ],
 })
 export class AppModule {}
