86442f0075298ed61412dd46a6fd9a74f667997c
MauroRaya <maurorayabr@gmail.com>
2026-05-09 15:53:46 -0300

    feat: add delete all method for user repository interface

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,8 +1,9 @@
 import { User } from 'src/user/user.entity';
-import { UpdateResult } from 'typeorm';
+import { DeleteResult, UpdateResult } from 'typeorm';
 
 export interface UserRepository {
   findOneByEmail(email: string): Promise<User | null>;
   updateAll(user: Partial<User>): Promise<UpdateResult>;
   save(user: Partial<User>): Promise<User>;
+  deleteAll(): Promise<DeleteResult>;
 }
